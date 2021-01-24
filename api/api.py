e120255152d222e399303e09321f94ade3621b87
RahulKanwal <rahulkanwal1999@gmail.com>
2021-01-24 18:51:20 +0530

    Development of the website almost done.

--- api/api.py
+++ api/api.py
@@ -1,8 +1,114 @@
-from flask import Flask
-import time
+from flask import Flask, request, jsonify
+import os
+import re
+import json
+from googleapiclient.discovery import build
+from datetime import timedelta
 
 app = Flask(__name__)
 
-@app.route('/duration')
-def get_current_time():
-    return {"time": time.time()}
+@app.route('/api/duration', methods=['POST'])
+def get_playlist_duration():
+    link = request.get_json()
+
+    # regular expression for getting playlist id from the youtube link.
+    playlist_pattern = re.compile(r'^(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?.*?(?:v|list)=(.*?)(?:&|$)|^(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?(?:(?!=).)*\/(.*)$')
+
+    # searching for playlist id in the playlist link data with the help of regular expressions.
+    playlist_link = playlist_pattern.search(link['link'])
+
+    # taking the playlist id data.
+    playlist_link = playlist_link.group(1)
+
+    # api key
+    api_key = os.getenv('API_KEY')
+
+    # getting youtube api with proper version using build.
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    # nextPageToken for getting playlists having more than 50 videos( for pagination)
+    nextPageToken = None
+
+    # regular expression for getting hours, minutes and seconds from the API Data of Duration
+    hours_pattern = re.compile(r'(\d+)H')
+    minutes_pattern = re.compile(r'(\d+)M')
+    seconds_pattern = re.compile(r'(\d+)S')
+
+    # variable for keeping the count of total number of seconds of the playlist.
+    total_seconds = 0
+
+    # while loop which does api calls till we get the durations of all the videos in the playlist.
+    while True:
+        # API request to get the details of the playlist.
+        pl_request = youtube.playlistItems().list(
+            part='contentDetails',
+            playlistId= playlist_link,
+            maxResults=50,
+            pageToken=nextPageToken
+        )
+
+        # executing the playlistItems request to get the response from the API.
+        pl_response = pl_request.execute()
+
+        # list which stores the video ids of all the videos of the playlist
+        vid_ids = []
+
+        # for loop to get the video ids in the vid_ids list.
+        for item in pl_response['items']:
+            vid_ids.append(item['contentDetails']['videoId'])
+
+        # API request to get the details about the videos.
+        vid_requests = youtube.videos().list(
+            part='contentDetails',
+            id=','.join(vid_ids)
+        )
+
+        # executing the videos request to get the response from the API.
+        vid_response = vid_requests.execute()
+
+        # for loop to calculate the total duration of all the videos
+        for item in vid_response['items']:
+            duration = item['contentDetails']['duration']
+
+            # searching for the hours, minutes and seconds in the duration data with the help of regular expressions.
+            hours = hours_pattern.search(duration)
+            minutes = minutes_pattern.search(duration)
+            seconds = seconds_pattern.search(duration)
+
+            # assigning hours, minutes and seconds variable to int and if it is not in the duration data then assigning it to zero value.
+            hours = int(hours.group(1)) if hours else 0
+            minutes = int(minutes.group(1)) if minutes else 0
+            seconds = int(seconds.group(1)) if seconds else 0
+
+            # using timedelta function to convert hours, minutes and seconds into total seconds duration of the video.
+            video_seconds = timedelta(
+                hours = hours,
+                minutes = minutes,
+                seconds = seconds
+            ).total_seconds()
+
+            # adding video seconds to get the total duration of playlist in seconds.
+            total_seconds += video_seconds
+
+        # getting the nextPageToken to see if there are more videos in the playlist.
+        nextPageToken = pl_response.get('nextPageToken')
+
+        # checking if there are more videos to get from the playlist.
+        if not nextPageToken:
+            break
+
+    # calculating hours, minutes and seconds from the total_seconds.
+    total_seconds = int(total_seconds)
+
+    minutes, seconds = divmod(total_seconds, 60)
+
+    hours, minutes = divmod(minutes, 60)
+
+    duration = str(hours)+':'+str(minutes)+':'+str(seconds)
+
+    print(duration)
+
+    return duration
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import ReactDOM from 'react-dom';
 
 function App() {
+  const [playlist, setPlaylist] = useState( "" );
+  // const result = useState( '' );
+  // const result = useRef(null);
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios.post('/api/duration', {"link": playlist})
+    .then(response => {
+      console.log(response)
+      // alert(response.data)
+      // result(response)
+      ReactDOM.render(<h2>Playlist Duration: {response.data}</h2>,
+      document.getElementById('duration'));
+    })
+    .catch(error => {
+      console.log(error)
+      ReactDOM.render(<p>Sorry. There is error. Please check the playlist link. If the link is correct then there must be an issue in the API and we are sorry about that.</p>,
+      document.getElementById('duration'));
+    })
+    setPlaylist("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>YouTube Playlist Duration</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter Playlist Link:
+          <input type="text" value={playlist} onChange={e => setPlaylist(e.target.value)} />
+        </label>
+        <input type="submit" name="Calculate" />
+      </form>
+      <div id="duration">
+
+      </div>
     </div>
   );
 }
